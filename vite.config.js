ca7b692859ab2a3748da21d3bcddb09fdf78139d
U1 <ue4761@gmail.com>
2025-11-30 13:32:58 +0800

    [modify] 更新 Vite 配置，設定基底路徑為 '/TangibleAI/'

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/TangibleAI/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
